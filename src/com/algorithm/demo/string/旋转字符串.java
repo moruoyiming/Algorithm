092f51a1bc66f328c6001fb2ba8f0625910cc476
jianruilin <798774875@qq.com>
2021-02-07 18:02:23 +0800

    算法学习

---
+++ src/com/algorithm/demo/string/旋转字符串.java
@@ -0,0 +1,83 @@
+package com.algorithm.demo.string;
+
+/**
+ * 8. 旋转字符串
+ * 给定一个字符串（以字符数组的形式给出）和一个偏移量，根据偏移量原地旋转字符串(从左向右旋转)。
+ * <p>
+ * 样例
+ * Example 1:
+ * <p>
+ * Input: str="abcdefg", offset = 3
+ * Output: str = "efgabcd"
+ * Explanation: Note that it is rotated in place, that is, after str is rotated, it becomes "efgabcd".
+ * Example 2:
+ * <p>
+ * Input: str="abcdefg", offset = 0
+ * Output: str = "abcdefg"
+ * Explanation: Note that it is rotated in place, that is, after str is rotated, it becomes "abcdefg".
+ * Example 3:
+ * <p>
+ * Input: str="abcdefg", offset = 1
+ * Output: str = "gabcdef"
+ * Explanation: Note that it is rotated in place, that is, after str is rotated, it becomes "gabcdef".
+ * Example 4:
+ * <p>
+ * Input: str="abcdefg", offset =2
+ * Output: str = "fgabcde"
+ * Explanation: Note that it is rotated in place, that is, after str is rotated, it becomes "fgabcde".
+ * Example 5:
+ * <p>
+ * Input: str="abcdefg", offset = 10
+ * Output: str = "efgabcd"
+ * Explanation: Note that it is rotated in place, that is, after str is rotated, it becomes "efgabcd".
+ * 挑战
+ * 在数组上原地旋转，使用O(1)的额外空间
+ * <p>
+ * 说明
+ * 原地旋转意味着你要在s本身进行修改。你不需要返回任何东西。
+ * <p>
+ * 注意事项
+ * offset >= 0
+ * the length of str >= 0
+ * Make changes on the original input data
+ */
+public class 旋转字符串 {
+
+    public static void main(String[] args) {
+        String str = "abcdefg"; // defgabc
+        rotateString(str.toCharArray(), 2);
+    }
+
+    /*
+     * param A: A string
+     * param offset: Rotate string with offset.
+     * return: Rotated string.
+     */
+    public static char[] rotateString(char[] A, int offset) {
+        if (A == null || A.length == 0) {
+            return A;
+        }
+
+        int len = A.length;
+        offset %= len;
+        System.out.println(" len1=" + len + " offset=" + offset + " " + (len - offset - 1));
+        reverse(A, 0, len - offset - 1);
+        System.out.println(" len2=" + len + " offset=" + offset + " (len - offset)" + (len - offset) + " (len - 1)" + (len - 1));
+        reverse(A, len - offset, len - 1);
+        System.out.println(" len3=" + len + " offset=" + offset + " (len - 1)" + (len - 1));
+        reverse(A, 0, len - 1);
+        return A;
+    }
+
+    private static void reverse(char[] str, int start, int end) {
+        System.out.println("reverse " + " start=" + start + " end=" + end);
+        while (start < end) {
+            char temp = str[start];
+            str[start] = str[end];
+            str[end] = temp;
+            start++;
+            end--;
+        }
+    }
+
+}
