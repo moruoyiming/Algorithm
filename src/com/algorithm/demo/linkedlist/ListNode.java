143410a00fd38d28746a9d56e750cae1c5de04ac
jian <moruoyiming123@gmail.com>
2021-02-16 17:13:15 +0800

    算法学习

--- src/com/algorithm/demo/linkedlist/ListNode.java
+++ src/com/algorithm/demo/linkedlist/ListNode.java
@@ -4,8 +4,16 @@ public class ListNode {
     int val;
     ListNode next;
 
-    ListNode(int x) {
+    public ListNode(int val, ListNode next) {
+        this.val = val;
+        this.next = next;
+    }
+
+    public ListNode(int x) {
         val = x;
         next = null;
     }
+
+    public ListNode() {}
+
 }
