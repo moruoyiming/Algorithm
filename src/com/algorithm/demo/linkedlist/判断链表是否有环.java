818fb1eaf34d6452bcfd32d472ac69956542875c
菅瑞霖 <ruilin.jian@cocos.com>
2021-04-30 14:25:37 +0800

    链表相关

--- src/com/algorithm/demo/linkedlist/判断链表是否有环.java
+++ src/com/algorithm/demo/linkedlist/判断链表是否有环.java
@@ -10,9 +10,8 @@ public class 判断链表是否有环 {
         if (head == null || head.next == null) {
             return false;
         }
-        ListNode fast, slow;
-        fast = head.next;
-        slow = head;
+        ListNode fast = head.next;
+        ListNode slow = head;
         while (fast != slow) {
             if(fast==null || fast.next==null)
                 return false;
