5bf7073914d9b5d58f0a2fdfec22569c35b18ad7
Jian <moruoyiming123@gmail.com>
2021-05-07 00:27:30 +0800

    字符串 面试题

---
+++ src/com/algorithm/demo/array/主元素.java
@@ -0,0 +1,53 @@
+package com.algorithm.demo.array;
+
+import java.util.List;
+
+/**
+ * 描述
+ * 给定一个整型数组，找出主元素，它在数组中的出现次数大于数组元素个数的二分之一。
+ * <p>
+ * 假设数组非空，且数组中总是存在主元素。
+ * <p>
+ * 样例
+ * 样例 1：
+ * <p>
+ * 输入：
+ * <p>
+ * 数组 = [1, 1, 1, 1, 2, 2, 2]
+ * 输出：
+ * <p>
+ * 1
+ * 解释：
+ * <p>
+ * 数组中1的个数大于数组元素的二分之一。
+ * <p>
+ * 样例 2：
+ * <p>
+ * 输入：
+ * <p>
+ * 数组 = [1, 1, 1, 2, 2, 2, 2]
+ * 输出：
+ * <p>
+ * 2
+ * 解释：
+ * <p>
+ * 数组中2的个数大于数组元素的二分之一。
+ * <p>
+ * 挑战
+ * 要求时间复杂度为O(n)，空间复杂度为O(1)
+ */
+public class 主元素 {
+
+    public static void main(String[] args) {
+        int result = majorityNumber();
+        System.out.println("result=" + result);
+    }
+
+    /*
+     * @param nums: a list of integers
+     * @return: find a  majority number
+     */
+    public static int majorityNumber(List<Integer> nums) {
+        // write your code here
+    }
+}
