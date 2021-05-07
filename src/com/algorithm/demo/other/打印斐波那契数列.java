8bd1d7b1ef7ed972c72b7c8f6fcc4eace1bc759a
菅瑞霖 <ruilin.jian@cocos.com>
2021-05-07 18:38:26 +0800

    斐波那契数

---
+++ src/com/algorithm/demo/other/打印斐波那契数列.java
@@ -0,0 +1,22 @@
+package com.algorithm.demo.other;
+
+public class 打印斐波那契数列 {
+
+    public static void main(String[] args) {
+        what(10);
+    }
+
+    public static void what(int n) {
+        int nums[] = new int[n];//产生一个整数数组
+        for (int i = 0; i < n; i++) {
+            if (i < 2) {
+                nums[i] = 1;//前两个数为1
+            } else {
+                nums[i] = nums[i - 1] + nums[i - 2];//第三个数开始等于前两个数之和
+            }
+            System.out.print(nums[i] + ",");//打印输出
+        }
+    }
+
+
+}
