4ae030041b495b2a61316125c64368f9e4e87ff4
Jian <moruoyiming123@gmail.com>
2021-05-25 21:49:08 +0800

    三位整数

--- src/com/algorithm/demo/interview/Q4_链表倒数第n个节点.java
+++ src/com/algorithm/demo/interview/Q4_链表倒数第n个节点.java
@@ -10,8 +10,22 @@ public class Q4_链表倒数第n个节点 {
      * @return: Nth to last node of a singly linked list.
      */
     public ListNode nthToLast(ListNode head, int n) {// 3  5  6  8  23
-
-        return null;
+        if (head == null || n < 1) {
+            return null;
+        }
+        ListNode p1 = head;
+        ListNode p2 = head;
+        for (int i = 0; i < n - 1; i++) {
+            if (p2 == null) {
+                return null;
+            }
+            p2 = p2.next;
+        }
+        while (p2 != null) {
+            p1 = p1.next;
+            p2 = p2.next;
+        }
+        return p1;
     }
 
 }
