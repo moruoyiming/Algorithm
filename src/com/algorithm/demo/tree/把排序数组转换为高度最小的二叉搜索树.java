befac773ab9946b0a04b6c8d4ce64f9d518fda0e
Jian <moruoyiming123@gmail.com>
2021-06-09 00:12:31 +0800

    继续学习算法

---
+++ src/com/algorithm/demo/tree/把排序数组转换为高度最小的二叉搜索树.java
@@ -0,0 +1,16 @@
+package com.algorithm.demo.tree;
+
+public class 把排序数组转换为高度最小的二叉搜索树 {
+
+    public static void main(String[] args) {
+
+    }
+
+    /*
+     * @param A: an integer array
+     * @return: A tree node
+     */
+    public TreeNode sortedArrayToBST(int[] A) {
+        // write your code here
+    }
+}
