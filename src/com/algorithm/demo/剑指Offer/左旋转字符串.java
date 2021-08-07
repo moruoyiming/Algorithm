3ecf35acd4d052602445f21389c31df4426bdde5
jian <jianruilin@100tal.com.cn>
2021-08-07 08:42:16 +0800

    剑指offer

---
+++ src/com/algorithm/demo/剑指Offer/左旋转字符串.java
@@ -0,0 +1,24 @@
+package com.algorithm.demo.剑指Offer;
+
+/**
+ * 字符串的左旋转操作是把字符串前面的若干个字符转移到字符串的尾部。请定义一个函数实现左旋转操作的功能呢。
+ * 例如：输入字符串"abcdefg"和数字2，该函数将返回左旋转两位得到的结果"cdefgab"。
+ * 可以将字符串分为两个部分，由于想把它的前两个字符移到后面，就把前两个字符分到第一部分，把后面的所有字符分到第二部分。
+ * 先分别旋转这两部分，得到"bagfedc"。接下来反转整个字符串，得到"cdefgab"刚好把原始字符串左旋转两位的结果。
+ */
+public class 左旋转字符串 {
+
+    public static void main(String[] args) {
+        String str = "abcdefg";
+        String result = leftRotateString(str, 2);
+        System.out.println("result=" + result);
+    }
+
+    public static String leftRotateString(String str, int n) {
+        if (str != null && !"".equals(str)) {
+
+        }
+        return str;
+    }
+
+}
