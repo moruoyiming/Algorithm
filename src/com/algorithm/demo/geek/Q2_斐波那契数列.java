29a4693b236d09abf30f07ffe1ba710b5787b8d0
jian <jianruilin@100tal.com.cn>
2021-11-09 10:57:25 +0800

    Geek 学习

---
+++ src/com/algorithm/demo/geek/Q2_斐波那契数列.java
@@ -0,0 +1,17 @@
+package com.algorithm.demo.geek;
+
+public class Q2_斐波那契数列 {
+
+    public static void main(String[] args) {
+
+    }
+    // f(n) = f(n-1) + f(n-2)
+    // 画出递归调用图
+    // f6 = f5 + f4
+    // f5 = f4+ f3
+    // f4 = f3 + f2
+    public static void faib(){
+//        递归调用会出现性能问题
+    }
+
+}
