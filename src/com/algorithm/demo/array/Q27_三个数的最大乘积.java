44a33e9df2f5171beb241ad1c596b6f3944dd01b
jian <jianruilin@100tal.com.cn>
2021-12-03 15:17:51 +0800

    Geek 学习

--- src/com/algorithm/demo/array/Q27_三个数的最大乘积.java
+++ src/com/algorithm/demo/array/Q27_三个数的最大乘积.java
@@ -29,21 +29,55 @@ public class Q27_三个数的最大乘积 {
     }
 
     /**
-     * @param nums: an integer array
-     * @return: the maximum product
+     * 方法一：排序
+     * 首先将数组排序。
+     * 如果数组中全是非负数，则排序后最大的三个数相乘即为最大乘积；如果全是非正数，则最大的三个数相乘同样也为最大乘积。
+     * 如果数组中有正数有负数，则最大乘积既可能是三个最大正数的乘积，也可能是两个最小负数（即绝对值最大）与最大正数的乘积。
+     * 综上，我们在给数组排序后，分别求出三个最大正数的乘积，以及两个最小负数与最大正数的乘积，二者之间的最大值即为所求答案。
+     * 时间复杂度：O(N\log N)O(NlogN)，其中 NN 为数组长度。排序需要 O(N\log N)O(NlogN) 的时间。
+     * 空间复杂度：O(\log N)O(logN)，主要为排序的空间开销。
+     *
+     * @param nums
+     * @return
      */
     private static int maximumProduct(int[] nums) {
-        // Write your code here
-        if (nums == null || nums.length <= 2) {
-            return 0;
-        }
         Arrays.sort(nums);
         int n = nums.length;
-        if (nums[0] * nums[1] < nums[n - 2] * nums[n - 3]) {
-            return nums[n - 1] * nums[n - 2] * nums[n - 3];
-        } else {
-            return nums[0] * nums[1] * nums[n - 1];
+        return Math.max(nums[0] * nums[1] * nums[n - 1], nums[n - 1] * nums[n - 2] * nums[n - 3]);
+    }
+
+    /**
+     * 方法二：线性扫描
+     * 在方法一中，我们实际上只要求出数组中最大的三个数以及最小的两个数，因此我们可以不用排序，用线性扫描直接得出这五个数。
+     * 时间复杂度：O(N)O(N)，其中 NN 为数组长度。我们仅需遍历数组一次。
+     * 空间复杂度：O(1)O(1)。
+     *
+     * @param nums
+     * @return
+     */
+    private static int maximumProduct2(int[] nums) {
+        int min1 = Integer.MAX_VALUE, min2 = Integer.MAX_VALUE;
+        int max1 = Integer.MIN_VALUE, max2 = Integer.MAX_VALUE, max3 = Integer.MAX_VALUE;
+        for (int x : nums) {
+            if (x < min1) {
+                min2 = min1;
+                min1 = x;
+            } else if (x < min2) {
+                min2 = x;
+            }
+            if (x > max1) {
+                max3 = max2;
+                max2 = max1;
+                max1 = x;
+            } else if (x > max2) {
+                max3 = max2;
+                max2 = x;
+            } else if (x > max3) {
+                max3 =x;
+            }
+
         }
+        return Math.max(min1 * min2 * max1, max1 * max2 * max3);
     }
 
 }
