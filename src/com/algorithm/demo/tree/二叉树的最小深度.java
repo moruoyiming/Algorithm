93e8186610071a0c2ca76fcd8b967cd4ceb928d9
Rui Lin  Jian <moruoyiming123@gmail.com>
2021-12-07 12:47:46 +0800

    suanfa

--- src/com/algorithm/demo/tree/二叉树的最小深度.java
+++ src/com/algorithm/demo/tree/二叉树的最小深度.java
@@ -44,4 +44,31 @@ public class 二叉树的最小深度 {
         return Math.min(left, right) + 1;
     }
 
+    /**
+     * 方法一：深度优先搜索
+     * 首先可以想到使用深度优先搜索的方法，遍历整棵树，记录最小深度。
+     * 对于每一个非叶子节点，我们只需要分别计算其左右子树的最小叶子节点深度。这样就将一个大问题转化为了小问题，可以递归地解决该问题。
+     * 时间复杂度：O(N)O(N)，其中 NN 是树的节点数。对每个节点访问一次。
+     * 空间复杂度：O(H)O(H)，其中 HH 是树的高度。空间复杂度主要取决于递归时栈空间的开销，最坏情况下，树呈现链状，空间复杂度为 O(N)O(N)。平均情况下树的高度与节点数的对数正相关，空间复杂度为 O(\log N)O(logN)。
+     *
+     * @param root
+     * @return
+     */
+    public int minDepth2(TreeNode root) {
+        if (root == null) {
+            return 0;
+        }
+        if (root.left == null || root.right == null) {
+            return 1;
+        }
+        int min_depth = Integer.MAX_VALUE;
+        if (root.left != null) {
+            min_depth = Math.min(minDepth2(root.left), min_depth);
+        }
+        if (root.right != null) {
+            min_depth = Math.min(minDepth2(root.right), min_depth);
+        }
+        return min_depth + 1;
+    }
+
 }
