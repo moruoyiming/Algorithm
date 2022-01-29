3690e051cd2411f8374b9224eccf2aa074b09e1f
Rui Lin  Jian <moruoyiming123@gmail.com>
2022-01-29 14:21:40 +0800

    算法学习

--- src/com/algorithm/demo/array/Q23_最大连续1的个数.java
+++ src/com/algorithm/demo/array/Q23_最大连续1的个数.java
@@ -46,4 +46,19 @@ public class Q23_最大连续1的个数 {
         return maxcount;
     }
 
+    public static int findMaxConsecutiveOnes2(int[] nums) {
+        int max = 0;
+        int count = 0;
+        for(int num : nums){
+            if(num == 1){
+                count++;
+                max = Math.max(max,count);
+            }else{
+                count = 0;
+            }
+        }
+        return max;
+    }
+
+
 }
