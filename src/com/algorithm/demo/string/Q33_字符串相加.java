aa8814ef91eb7fb65913dcc112bb51c4940e32d2
Rui Lin  Jian <moruoyiming123@gmail.com>
2022-03-30 17:15:53 +0800

    算法学习

---
+++ src/com/algorithm/demo/string/Q33_字符串相加.java
@@ -0,0 +1,41 @@
+package com.algorithm.demo.string;
+
+/**
+ * 415. 字符串相加
+ * 给定两个字符串形式的非负整数 num1 和num2 ，计算它们的和并同样以字符串形式返回。
+ * 你不能使用任何內建的用于处理大整数的库（比如 BigInteger）， 也不能直接将输入的字符串转换为整数形式。
+ * 示例 1：
+ * 输入：num1 = "11", num2 = "123"
+ * 输出："134"
+ * 示例 2：
+ * 输入：num1 = "456", num2 = "77"
+ * 输出："533"
+ * 示例 3：
+ * 输入：num1 = "0", num2 = "0"
+ * 输出："0"
+ */
+public class Q33_字符串相加 {
+
+    public String addStrings(String num1, String num2) {
+        StringBuilder sb = new StringBuilder();
+        int i = num1.length() - 1;
+        int j = num2.length() - 1;
+        int carry = 0;
+        while (i >= 0 || j >= 0) {
+            int a = i >= 0 ? num1.charAt(i) - '0' : 0;
+            int b = j >= 0 ? num2.charAt(j) - '0' : 0;
+            int temp = a + b + carry;
+            carry = temp / 10;
+            sb.append(temp % 10);
+            i--;
+            j--;
+        }
+        if (carry == 1) {
+            sb.append(carry);
+        }
+        return sb.reverse().toString();
+    }
+
+    ;
+
+}
