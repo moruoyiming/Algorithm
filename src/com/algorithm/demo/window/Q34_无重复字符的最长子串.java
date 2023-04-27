e769c240aa15e86904d49edc29cc598352e155c0
TAL-JIAN <jianruilin@tal.com>
2023-04-27 15:50:44 +0800

    滑动窗口

---
+++ src/com/algorithm/demo/window/Q34_无重复字符的最长子串.java
@@ -0,0 +1,61 @@
+package com.algorithm.demo.window;
+
+import java.util.HashMap;
+
+/**
+ * 3. 无重复字符的最长子串
+ * 给定一个字符串 s ，请你找出其中不含有重复字符的 最长子串 的长度。
+ * <p>
+ * <p>
+ * <p>
+ * 示例 1:
+ * <p>
+ * 输入: s = "abcabcbb"
+ * 输出: 3
+ * 解释: 因为无重复字符的最长子串是 "abc"，所以其长度为 3。
+ * 示例 2:
+ * <p>
+ * 输入: s = "bbbbb"
+ * 输出: 1
+ * 解释: 因为无重复字符的最长子串是 "b"，所以其长度为 1。
+ * 示例 3:
+ * <p>
+ * 输入: s = "pwwkew"
+ * 输出: 3
+ * 解释: 因为无重复字符的最长子串是 "wke"，所以其长度为 3。
+ * 请注意，你的答案必须是 子串 的长度，"pwke" 是一个子序列，不是子串。
+ */
+public class Q34_无重复字符的最长子串 {
+
+    public static void main(String[] args) {
+
+    }
+
+    /**
+     * 思路：
+     * 这道题主要用到思路是：滑动窗口
+     * 什么是滑动窗口？
+     * 其实就是一个队列,比如例题中的 abcabcbb，进入这个队列（窗口）为 abc 满足题目要求，当再进入 a，队列变成了 abca，这时候不满足要求。所以，我们要移动这个队列！
+     * 如何移动？
+     * 我们只要把队列的左边的元素移出就行了，直到满足题目要求！
+     * 一直维持这样的队列，找出队列出现最长的长度时候，求出解！
+     * 时间复杂度：O(n)
+     */
+    public int lengthOfLongestSubstring(String s) {
+        if (s.length() == 0) {
+            return 0;
+        }
+        HashMap<Character, Integer> map = new HashMap<>();
+        int max = 0;
+        int left = 0;
+        for (int i = 0; i < s.length(); i++) {
+            if (map.containsKey(s.charAt(i))) {
+                left = Math.max(left, map.get(s.charAt(i)) + 1);
+            }
+            map.put(s.charAt(i), i);
+            max = Math.max(max, i - left + 1);
+        }
+        return max;
+    }
+
+}
